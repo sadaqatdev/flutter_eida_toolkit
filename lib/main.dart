d0eb7ad9140c0a913d0c2ea495436ed984ae55fa
Macbook Pro <macbookpro@Macbooks-MacBook-Pro.local>
2024-01-03 09:50:42 +0500

    save
    save

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Graba Test 1'),
+      home: const MyHomePage(title: 'Grabba Test 3'),
     );
   }
 }
